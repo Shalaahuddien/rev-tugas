44f737e08411dc47b6f323da25424bc087b60a84
YOUR NAME <YOUR EMAIL@SAMLECOM>
2022-12-12 13:13:34 +0700

    practice

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'withAuth' => \App\Http\Middleware\withAuth::class,
+        'noAuth' => \App\Http\Middleware\NoAuth::class,
     ];
 }
